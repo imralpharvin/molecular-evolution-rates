1080f54980a5b230b31411ed6678d51f782c14ee
Ralph Arvin De Castro <rdecastr@uoguelph.ca>
2018-06-08 15:58:10 -0400

    updated

--- genbank/Sec1GenbankParserSeachTerm.R
+++ genbank/Sec1GenbankParserSeachTerm.R
@@ -5,41 +5,38 @@ library(ape)
 library("Biostrings")
 library (rentrez)
 
-searchTerm <- "cytochrome b[All Fields] AND \"Mammalia\"[Organism]"
-numSearch <- 1000
-searchResult <- entrez_search(db="nuccore", term=searchTerm, retmax=numSearch)
+#Put gene and organism here
+searchTerm <- "cytochrome b[All Fields] AND \"Sus\"[Organism]"
+#Put how many searches
+#search genbank
+searchResult <- entrez_search(db="nuccore", term=searchTerm)
 searchResult
+numSearch <- searchResult$count
+searchResult <- entrez_search(db="nuccore", term=searchTerm, retmax= numSearch)
+searchResult
+
+
+#Setting up boundaries
 tempMin = 1
 tempMax = 300
 
-searchSequences <- entrez_fetch(db="nuccore", id=searchResult$ids[tempMin:tempMax] ,rettype="fasta")
-write(searchSequences, "searchSequences.fasta", sep="\n")
+if(numSearch < 300){
+  tempMax <- numSearch
+  searchSequences <- entrez_fetch(db="nuccore", id=searchResult$ids[tempMin:tempMax] ,rettype="fasta")
+  write(searchSequences, "searchSequences.fasta", sep="\n")
+} else{
+  searchSequences <- entrez_fetch(db="nuccore", id=searchResult$ids[tempMin:tempMax] ,rettype="fasta")
+  write(searchSequences, "searchSequences.fasta", sep="\n")
 
-while(tempMax != numSearch){
-  tempMin = tempMin + 300
-  tempMax = tempMax + 300
+  while(tempMax != numSearch){
+    tempMin = tempMin + 300
+    tempMax = tempMax + 300
   if(numSearch <= tempMax ){
     tempMax = numSearch
   }
-  #commented out
-  if(FALSE){
-  if(tempMin < 798 && tempMax > 798) {
-  print("stucks")
-  searchSequences <- entrez_fetch(db="nuccore", id=searchResult$ids[tempMin:797] ,rettype="fasta")
-  write(searchSequences, "searchSequences.fasta", sep="\n", append = TRUE)
-  
-  searchSequences <- entrez_fetch(db="nuccore", id=searchResult$ids[799: tempMax] ,rettype="fasta")
-  #write(searchSequences, "searchSequences.fasta", sep="\n", append = TRUE)
-  }else if(tempMax == 798){
-    searchSequences <- entrez_fetch(db="nuccore", id=searchResult$ids[tempMin:797] ,rettype="fasta")
-    write(searchSequences, "searchSequences.fasta", sep="\n", append = TRUE)
-  } 
-  #commented out
-  }
-  else{
   searchSequences <- entrez_fetch(db="nuccore", id=searchResult$ids[tempMin:tempMax] ,rettype="fasta")
   write(searchSequences, "searchSequences.fasta", sep="\n", append = TRUE)
-  }
+}
 }
 
 cytB <- readDNAStringSet("searchSequences.fasta")
