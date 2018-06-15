ab9c51af400984d5fb5b6278de68de889405e688
Ralph Arvin De Castro <rdecastr@uoguelph.ca>
2018-06-15 11:36:23 -0400

    updated

--- mammals/rscripts/Sec2.R
+++ mammals/rscripts/Sec2.R
@@ -19,17 +19,30 @@ dfLatitudeSpecies <- dfFiltered[grep("[0-9]", lat)]
 dfLatitudeSpecies[, lat_num := as.numeric(lat)]
 #3. Conversion to absolute values before median latitude values are calculated.
 dfLatitudeSpecies[, abs_lat_num := abs(lat_num)]
+
 #4. Determine a median latitude for each BIN using absolute values.
 dfLatitudeSpecies[, median_lat := median(abs_lat_num), keyby = bin_uri]
+#Range
+dfLatitudeSpecies[, max_lat := max(lat_num), keyby = bin_uri]
+dfLatitudeSpecies[, min_lat := min(lat_num), keyby = bin_uri]
+
+dfLatitudeSpecies[, range_lat := max_lat - min_lat, keyby = bin_uri]
+dfLatitudeSpecies <- dfLatitudeSpecies[, !c(14,15,17,18)]
 
 # While considering traits for eventual multivariate analyses, it is necessary for them to have an adequate sample size 
 # (i.e. over x # rows of data, depending on your purposes). In addition, they should exhibit some amount of variation across the observations.
 
 # Use the GetTraitSpecificDataBIN function to obtain a subset of data for those species that have latitude data available.
 #5. Get the trait specific datatable.
-dfLatitude <- setDT(GetTraitSpecificDataBIN(dfLatitudeSpecies, 16))
+dfLatitudeMedian <- setDT(GetTraitSpecificDataBIN(dfLatitudeSpecies, 14))
+dfLatitudeRange <- setDT(GetTraitSpecificDataBIN(dfLatitudeSpecies, 15))
+setnames(dfLatitudeMedian, "species_label", "species_name")
+setnames(dfLatitudeRange, "species_label", "species_name")
+
 #6. Get information for the trait.
-GetTraitInfo(dfLatitude, "median_lat", type = "continuous")
+GetTraitInfo(dfLatitudeMedian, "median_lat", type = "continuous")
+GetTraitInfo(dfLatitudeRange, "range_lat", type = "continuous")
+
 
 #7. Datatable reorganization for dfFiltered.
 dfFiltered <- dfFiltered[, .(bin_uri, filtered_bin_size, recordID, order_name = order_label, family_name = family_label, genus_name = genus_label,
@@ -52,6 +65,9 @@ traitData[traitData == -999] <- NA
 
 #6 Converting to data table
 traitData <- as.data.table(traitData)
+traitData <- traitData[,!c(1,2,3)]
+dfLatitudeMedian <- dfLatitudeMedian[, !c(1,3)]
+dfLatitudeRange <- dfLatitudeRange[, !c(1,3)]
 
 #7 Merge Species name
 mergedSpecies <- merge(dfFiltered, traitData, by = "species_name")
@@ -60,26 +76,26 @@ mergedSpecies <- merge(dfFiltered, traitData, by = "species_name")
 dfFilteredSingle <- dfFiltered[!duplicated(species_name)][, .(bin_uri, species_name, filtered_bin_size)]
 
 #8 Merge with latitude
-dfTraits <- merge(dfFilteredSingle, dfLatitude, all = T, by = "bin_uri")
+#dfTraits <- merge(dfFilteredSingle, dfLatitude, all = T, by = "bin_uri")
 
 
 #9 
-dfTraits <- Reduce(function(...) merge(..., all = T, by = "species_name"), list(dfTraits, traitData))
+dfTraits <- Reduce(function(...) merge(..., all = T, by = "species_name"), list(dfFilteredSingle, dfLatitudeMedian,dfLatitudeRange, traitData ))
 
 #10
 dfTraits<- dfTraits[!is.na(bin_uri)]
 
-#11
-dfTraits <- dfTraits[, c(1:3, 6:20)]
-
 #12
-missing <- dfTraits[, apply(.SD, 1, function(x) all(is.na(x))), .SDcols = 4:13]
+missing <- dfTraits[, apply(.SD, 1, function(x) all(is.na(x))), .SDcols = 4:16]
 missing <- which(missing == TRUE)
 dfTraits <-dfTraits[!missing]
+
 
 dfPreCentroid <- merge(dfFiltered, dfTraits, by = "bin_uri")[, 1:8]
 dfPreCentroid<- dfPreCentroid[,c(7,1,2,3,4,5,6,8)]
 # Dataframe reorganization and renaming.
 setnames(dfPreCentroid, "species_name.x", "species_name")
+setnames(dfPreCentroid, "filtered_bin_size.x", "filtered_bin_size")
 
-rm(selectedTraits, missing); rm(dfLatitude, dfLatitudeSpecies, mergedSpecies, rawMammalData, dfFilteredSingle, dfResolve)
+
+rm(selectedTraits, missing); rm(traitData,dfLatitudeMedian,dfLatitudeRange, dfLatitudeSpecies, mergedSpecies, rawMammalData, dfFilteredSingle, dfResolve)
